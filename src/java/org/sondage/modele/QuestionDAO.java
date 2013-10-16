8531eb65272486c5329eb2fa6ee419c274d114e2
sofiane <sofiane@sofiane-ThinkPad-E420>
2013-10-16 17:37:41 -0400

    MAJ findALL

---
+++ src/java/org/sondage/modele/QuestionDAO.java
@@ -0,0 +1,184 @@
+package org.sondage.modele;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+public class QuestionDAO extends DAO<Question> {
+
+	public QuestionDAO(Connection c)
+	{
+		super(c);
+	}
+        
+	@Override
+	public boolean create(Question x) {
+		// TODO Auto-generated method stub
+	
+		String req = "INSERT INTO question (`NOMQUESTION` , `TEXTEQUESTION` , `IDQUESTIONNAIRE`) "+
+			     "VALUES ('"+x.getNomQuestion()+"','"+x.getTexteQuestion()+"','"+x.getIdQuestionnaire()+"')";
+
+		//System.out.println("REQUETE "+req);
+
+		Statement stm = null;
+		try 
+		{
+			stm = cnx.createStatement(); 
+			int n= stm.executeUpdate(req);
+			if (n>0)
+			{
+				stm.close();
+				return true;
+			}
+		}
+		catch (SQLException exp)
+		{
+		}
+		finally
+		{
+			if (stm!=null)
+			try {
+				stm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+
+		return false;
+	}
+        
+	@Override
+	public boolean delete(Question x) {
+		// TODO Auto-generated method stub
+		Statement stm = null;
+		try 
+		{
+			stm = cnx.createStatement(); 
+			int n= stm.executeUpdate("DELETE FROM question WHERE idquestion='"+x.getIdQuestion()+"'");
+			if (n>0)
+			{
+				stm.close();
+				return true;
+			}
+		}
+		catch (SQLException exp)
+		{
+		}
+		finally
+		{
+			if (stm!=null)
+			try {
+				stm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		return false;
+	}
+
+	@Override
+	public Question read(String id) {
+		// TODO Auto-generated method stub
+                Statement stm = null;
+                ResultSet r = null;
+		try 
+		{
+			stm = cnx.createStatement(); 
+			r = stm.executeQuery("SELECT * FROM question WHERE nomquestion = '"+id+"'");
+			if (r.next())
+			{
+				Question q = new Question(r.getString("nomquestion"),
+						r.getString("textequestion"),
+						Integer.parseInt(r.getString("idquestionnaire")));
+				q.setIdQuestion(Integer.parseInt(r.getString("idquestion")));
+                                r.close();
+				stm.close();
+				return q;
+			}
+		}
+		catch (SQLException exp)
+		{
+		}
+		finally
+		{
+			if (stm!=null)
+			try {
+                                r.close();
+				stm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		return null;
+	}
+
+	@Override
+	public boolean update(Question x) {
+		// TODO Auto-generated method stub
+		Statement stm = null;
+		try 
+		{
+			String req = "UPDATE questionnaire SET NOMQUESTION = '"+x.getNomQuestion()+"',"+
+                                                        "TEXTEQUESTION = '"+x.getTexteQuestion()+"'," +
+                                                        "IDQUESTIONNAIRE = '"+x.getIdQuestionnaire()+"'," +
+							" WHERE IDQUESTION = '"+x.getIdQuestion()+"'";
+			//System.out.println("REQUETE "+req);
+			stm = cnx.createStatement(); 
+			int n= stm.executeUpdate(req);
+			if (n>0)
+			{
+				stm.close();
+				return true;
+			}
+		}
+		catch (SQLException exp)
+		{
+		}
+		finally
+		{
+			if (stm!=null)
+			try {
+				stm.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		return false;
+	}
+	@Override
+	public List<Question> findAll() {
+		// TODO Auto-generated method stub
+		List<Question> liste = new LinkedList<Question>();
+		try 
+		{
+			Statement stm = cnx.createStatement(); 
+			ResultSet r = stm.executeQuery("SELECT * FROM question");
+			while (r.next())
+			{
+				Question q = new Question(r.getString("nomquestion"),
+						r.getString("textequestion"),
+						Integer.parseInt(r.getString("idquestionnaire")));
+                                q.setIdQuestion(Integer.parseInt(r.getString("idquestion")));
+				liste.add(q);
+			}
+			r.close();
+			stm.close();
+		}
+		catch (SQLException exp)
+		{
+		}
+		return liste;
+	}
+        
+    @Override
+    public Question read(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
