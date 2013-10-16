c351c4670d3a59bb12269ca4dccdcdeeec5d42b5
sofiane <sofiane@sofiane-ThinkPad-E420>
2013-10-16 15:31:27 -0400

    Ajout de idQuestionnaire

--- src/java/org/sondage/modele/Question.java
+++ src/java/org/sondage/modele/Question.java
@@ -10,11 +10,12 @@ package org.sondage.modele;
  */
 public class Question {
     String nomQuestion, texteQuestion;
-    int idQuestion; 
+    int idQuestion, idQuestionnaire; 
 
-    public Question(String nomQuestion, String texteQuestion) {
+    public Question(String nomQuestion, String texteQuestion, int idQuestionnaire) {
         this.nomQuestion = nomQuestion;
         this.texteQuestion = texteQuestion;
+        this.idQuestionnaire = idQuestionnaire;
     }
 
     public String getNomQuestion() {
@@ -41,6 +42,14 @@ public class Question {
         this.idQuestion = idQuestion;
     }
 
+    public int getIdQuestionnaire() {
+        return idQuestionnaire;
+    }
+
+    public void setIdQuestionnaire(int idQuestionnaire) {
+        this.idQuestionnaire = idQuestionnaire;
+    }
+    
     @Override
     public String toString() {
         return "Question{" + "nomQuestion=" + nomQuestion + ", texteQuestion=" + texteQuestion + ", idQuestion=" + idQuestion + '}';
