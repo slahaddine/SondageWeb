c91ee7c0dbdc6532551aa49410f534736b01d7ad
yanis <yanis@yanis-Lenovo-U410>
2013-10-16 16:15:12 -0400

    Questionnaire up to date

--- src/java/org/sondage/modele/Questionnaire.java
+++ src/java/org/sondage/modele/Questionnaire.java
@@ -10,7 +10,7 @@ package org.sondage.modele;
  */
 public class Questionnaire {
     private Question[] questions; 
-    private int nbQuestions, currentQuestion;;
+    private int nbQuestions, currentQuestion, idQuestionnaire; 
     private String nomSondage; 
     public Questionnaire(int nbQuestion, String _nomSondage) {
         questions = new Question[nbQuestion]; 
@@ -23,6 +23,15 @@ public class Questionnaire {
         else 
             return null; 
     }
+
+    public int getIdQuestionnaire() {
+        return idQuestionnaire;
+    }
+
+    public void setIdQuestionnaire(int idQuestionnaire) {
+        this.idQuestionnaire = idQuestionnaire;
+    }
+    
     public boolean setQuestion(Question questionToAdd){
         if((currentQuestion >= 0) && (currentQuestion < nbQuestions)){
             questions[currentQuestion] = questionToAdd; 
@@ -31,5 +40,21 @@ public class Questionnaire {
         }
         return false; 
     } 
+
+    public int getNbQuestions() {
+        return nbQuestions;
+    }
+
+    public void setNbQuestions(int nbQuestions) {
+        this.nbQuestions = nbQuestions;
+    }
+
+    public String getNomSondage() {
+        return nomSondage;
+    }
+
+    public void setNomSondage(String nomSondage) {
+        this.nomSondage = nomSondage;
+    }
     
 }
