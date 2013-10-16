14695512c2f1d319e823e8dbaba6c5179610d074
yanis <yanis@yanis-Lenovo-U410>
2013-10-16 17:32:44 -0400

    Reponse class up to date & ReponseDAO finished.

--- src/java/org/sondage/modele/Reponse.java
+++ src/java/org/sondage/modele/Reponse.java
@@ -11,7 +11,7 @@ package org.sondage.modele;
 public class Reponse {
     String nomReponse, texteReponse;
     boolean choisie;
-    int idQuestion; 
+    int idQuestion, idReponse; 
 
     public Reponse(String nomReponse, String texteReponse, int idQuestion) {
         this.nomReponse = nomReponse;
@@ -19,7 +19,6 @@ public class Reponse {
         this.choisie = false;
         this.idQuestion = idQuestion;
     }
-
     public String getNomReponse() {
         return nomReponse;
     }
@@ -30,6 +29,14 @@ public class Reponse {
 
     public String getTexteReponse() {
         return texteReponse;
+    }
+
+    public int getIdReponse() {
+        return idReponse;
+    }
+
+    public void setIdReponse(int idReponse) {
+        this.idReponse = idReponse;
     }
 
     public void setTexteReponse(String texteReponse) {
