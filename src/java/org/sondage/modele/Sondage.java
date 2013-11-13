43f1e55a95deaf3e9cb9ac640a0541cb23046c9e
r00tKill <Yanis@yaniss-mbp.int.crosemont.qc.ca>
2013-11-13 15:29:46 -0500

    test sondage

--- src/java/org/sondage/modele/Sondage.java
+++ src/java/org/sondage/modele/Sondage.java
@@ -9,14 +9,33 @@ package org.sondage.modele;
  * @author Usager
  */
 public class Sondage {
-    private String nomSondage;
+    private String nomSondage, description, otherName;
     private int idSondage;
     
     
 
-    public Sondage(String _nomSondage) {
+    public Sondage(String _nomSondage, String _description, String _otherName) {
         this.setNomSondage(_nomSondage); 
+        this.setDescription(_description);
+        this.setOtherName(_otherName);
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOtherName() {
+        return otherName;
+    }
+
+    public void setOtherName(String otherName) {
+        this.otherName = otherName;
+    }
+    
     public String getNomSondage(){
         return nomSondage; 
     }
